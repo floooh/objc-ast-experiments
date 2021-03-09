5be3ac4c6255b49e4e025ca5e0974363716a1d85
John Scott <john@jjrscott.com>
2021-03-09 12:34:06 +0000

    add header generation example

--- test/build.sh
+++ test/build.sh
@@ -1,2 +1,6 @@
+pushd ..
+python3 gen_ir.py  test.json test_ir.json
+python3 gen_c.py test_ir.json test/hello_macos.h
+popd
 clang hello.c -o hello_c -framework Cocoa
 clang hello.m -o hello_objc -framework Cocoa
